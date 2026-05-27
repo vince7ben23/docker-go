79c2d623e56bb8126df4069b1b7fab21586824c8
benben <vince7ben23@gmail.com>
2026-05-27 18:00:24 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,6 +13,10 @@ var _ = exec.Command
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 
+	// fmt.Println("os.Args[0]: ", os.Args[0])
+	// fmt.Println("os.Args[1]: ", os.Args[1])
+	// fmt.Println("os.Args[2]: ", os.Args[2])
+	// fmt.Println("os.Args[3:]: ", os.Args[3:])
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
